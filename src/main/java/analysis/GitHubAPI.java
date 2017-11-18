5db03a4d07702e3e38ab7806a2420fbda4ab08b3
yamorist <leiaorgana228@gmail.com>
2017-11-18 22:50:44 +0200

    Added class with API usage

---
+++ src/main/java/analysis/GitHubAPI.java
@@ -0,0 +1,85 @@
+package analysis;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
+/**
+ * This class provides methods for computing most starred and most committed repositories.
+ */
+public class GitHubAPI {
+    private final String token = "afd599b6d6536776169ab509c38e02feae0053d3";
+    private URI uri;
+
+    public ArrayList<RepositoryInfo> getMostStarredRepos(String sinceYYYYMMDD, String untilYYYYMMDD) {
+        try {
+            uri = new URIBuilder()
+                    .setScheme("https")
+                    .setHost("api.github.com")
+                    .setPath("/search/repositories")
+                    .setParameter("q", "stars:10000..*")
+                    .setParameter("q", "created:"+sinceYYYYMMDD+".."+untilYYYYMMDD)
+                    .setParameter("sort", "stars")
+                    .setParameter("order", "desc")
+                    .build();
+
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+            HttpGet httpget = new HttpGet(uri);
+            System.out.println(uri);
+            httpget.setHeader("Authorization", "token " + token);
+            httpget.setHeader("Accept", "application/vnd.github.v3+json");
+
+            CloseableHttpResponse response = httpClient.execute(httpget);
+            HttpEntity httpEntity = response.getEntity();
+
+            ArrayList<RepositoryInfo> listOfStarredRepos = new OrgJSONConverter().computeMostStarredRepos(EntityUtils.toString(httpEntity));
+            return listOfStarredRepos;
+        }
+        catch (IOException | URISyntaxException e) { e.printStackTrace(); }
+        catch (JSONException e) {
+            System.out.println("Objects were not found in specified period of time.");
+        }
+
+    return null;
+    }
+
+    public ArrayList<RepositoryInfo> getMostCommittedRepos(String sinceYYYYMMDD, String untilYYYYMMDD) {
+        try {
+            uri = new URIBuilder()
+                    .setScheme("https")
+                    .setHost("api.github.com")
+                    .setPath("/search/repositories")
+                    .setParameter("q", "created:"+sinceYYYYMMDD+".."+untilYYYYMMDD)
+                    .build();
+
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+            HttpGet httpget = new HttpGet(uri);
+            System.out.println(uri);
+            httpget.setHeader("Authorization", "token " + token);
+            httpget.setHeader("Accept", "application/vnd.github.v3+json");
+
+            CloseableHttpResponse response = httpClient.execute(httpget);
+            HttpEntity httpEntity = response.getEntity();
+
+            ArrayList<RepositoryInfo> listOfCommittedRepos = new OrgJSONConverter().computeMostCommittedRepos(EntityUtils.toString(httpEntity));
+            return listOfCommittedRepos;
+        }
+        catch (IOException | URISyntaxException e) { e.printStackTrace(); }
+        catch (JSONException e) {
+            System.out.println("Objects were not found in specified period of time.");
+        }
+
+        return null;
+    }
+
+}
